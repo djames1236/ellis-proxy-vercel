131285ae220e2de80bfb264136bdef452fbad6c3
djames1236 <djames1236@gmail.com>
2025-06-22 13:06:20 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,12 +1,32 @@
 import fetch from 'node-fetch';
 
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).send('Method Not Allowed');
     return;
   }
 
-  const { summary, description = '', year, month, day, hour, minute, duration } = req.body;
+  let rawBody = '';
+  for await (const chunk of req) {
+    rawBody += chunk;
+  }
+
+  let body;
+  try {
+    body = JSON.parse(rawBody);
+  } catch (err) {
+    console.error('Invalid JSON');
+    res.status(400).send('Invalid JSON');
+    return;
+  }
+
+  const { summary, description = '', year, month, day, hour, minute, duration } = body;
 
   if (!summary || !year || !month || !day || !hour || !minute || !duration) {
     res.status(400).send('Missing required parameters.');
